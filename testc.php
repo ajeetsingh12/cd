d83d34cba4f026793448dcd05f7611dd56aaf0bf
ajeetsingh12 <62153367+ajeetsingh12@users.noreply.github.com>
2020-08-28 16:55:41 +0530

    Update testc.php

--- testc.php
+++ testc.php
@@ -2,7 +2,7 @@
 $ToEmail = 'kaushiksir01@gmail.com'; 
 $EmailSubject = 'Site contact form'; 
 $mailheader = "From: ".$_POST["email"]."\r\n"; 
-$mailheader .= "Reply-To: ".$_POST["email"]."\r\n"; 
+
 $mailheader .= "Content-type: text/html; charset=iso-8859-1\r\n"; 
 $MESSAGE_BODY = "Name: ".$_POST["name"].""; 
 $MESSAGE_BODY .= "Email: ".$_POST["email"].""; 
